28954da89f1fcaeb54e39dac9a37d2d78207db8e
Alexander Sharov <asharov@griddynamics.com>
2018-11-10 01:39:36 +0400

    Add classifier to deploy projects separately

--- src/main/groovy/com/kvendingoldo/jdcl/core/JobGenerator.groovy
+++ src/main/groovy/com/kvendingoldo/jdcl/core/JobGenerator.groovy
@@ -11,52 +11,59 @@ import org.yaml.snakeyaml.*
 
 class JobGenerator {
     def dslFactory
+    def classifier
     def workspace
     def logger
 
-    public JobGenerator(dslFactory) {
+    JobGenerator() {}
+
+    JobGenerator(dslFactory, classifier) {
         this.dslFactory = dslFactory
+        this.classifier = classifier
         this.workspace = Executor.currentExecutor().getCurrentWorkspace()
         this.logger = new JenkinsLogger('INFO', this.dslFactory.out)
-
     }
 
     def generate() {
-        String configText = this.dslFactory.readFileFromWorkspace('configuration/job-generator.yml')
+        String configText = this.dslFactory.readFileFromWorkspace("configuration/${this.classifier}/job-generator.yml")
         (new Yaml().load(configText)).jobTypes.each { folder ->
             generate(folder)
         }
     }
 
     def loadBuildClass(def bc) {
-        return dslFactory.class.classLoader.parseClass(dslFactory.readFileFromWorkspace("jobdsl/${bc.job.type}/jobs/${bc.job.baseClassName}.groovy"))?.newInstance()
+        return this.dslFactory.class.classLoader.parseClass(dslFactory.readFileFromWorkspace("jobdsl/${bc.job.type}/jobs/${bc.job.baseClassName}.groovy"))?.newInstance()
     }
 
     def generate(type) {
         def configs = new FilePath(workspace, "jobdsl/${type}/configuration")
-        def configProcessor = new ConfigProcessor(dslFactory)
+        def configProcessor = new ConfigProcessor(this.dslFactory)
 
         configs.list().each { config ->
             logger.printLog "Reading: ${config}"
+
             configProcessor.processConfig(config.toString()).each { jobName, jc ->
                 if (jc.job.folder != '') {
                     def list = jc.job.folder.split("/").toList()
                     def folderName = "${list[0]}"
-                    dslFactory.folder(folderName)
+                    this.dslFactory.folder(folderName)
 
                     for (String item : list.drop(1)) {
                         folderName = folderName + "/" + item
-                        dslFactory.folder(folderName)
+                        this.dslFactory.folder(folderName)
                     }
                 }
 
-                def createJobs = { jcLocal ->
-                    logger.printLog "Processing..."
-                    ConfigProcessor.prettyPrint(jcLocal, this.dslFactory.out)
-                    def jobClass = loadBuildClass(jcLocal)
-                    jobClass.job(dslFactory, jcLocal)
+                if ((jc.job).containsKey('classifier') && jc.job.classifier == this.classifier) {
+                    def createJobs = { jcLocal ->
+                        logger.printLog "Processing..."
+                        ConfigProcessor.prettyPrint(jcLocal, this.dslFactory.out)
+                        def jobClass = loadBuildClass(jcLocal)
+                        jobClass.job(this.dslFactory, jcLocal)
+                    }
+                    createJobs(jc)
                 }
-                createJobs(jc)
+
             }
         }
     }
