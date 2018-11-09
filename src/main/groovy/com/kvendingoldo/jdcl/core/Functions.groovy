b2a4fa161694db5e45a9ab2a024698416ebbdd53
Alexander Sharov <kvendingoldo@yandex.ru>
2018-11-09 17:08:43 +0400

    Revert "Add ability to deploy from feature branches"
    This reverts commit 262c80968d8c82a06a48a12b6267f83af5e5f17c.

--- src/main/groovy/com/kvendingoldo/jdcl/core/Functions.groovy
+++ src/main/groovy/com/kvendingoldo/jdcl/core/Functions.groovy
@@ -1,20 +1,16 @@
 package com.kvendingoldo.jdcl.core
 
 static generateJobName(def jc) {
-    return generateJobName(jc.jenkins.mainJobDslBranch, jc.job.folder, jc.job.baseName)
+    return generateJobName(jc.job.folder, jc.job.baseName)
 }
 
-static generateJobName(String branch, String folder, String baseName) {
+static generateJobName(String folder, String baseName, String classifier = null) {
     String result = baseName
     if (folder != '') {
-        def build = Thread.currentThread().executable
-        def currentBranch = build.buildVariableResolver.resolve('BRANCH')
-
-        if (currentBranch != branch) {
-            folder = "${currentBranch}/${folder}"
-        }
-
         result = folder + '/' + baseName
+    }
+    if (classifier != null && classifier != '') {
+        result = "${result}_${classifier}"
     }
     return result
 }
--- src/main/groovy/com/kvendingoldo/jdcl/core/JobGenerator.groovy
+++ src/main/groovy/com/kvendingoldo/jdcl/core/JobGenerator.groovy
@@ -4,7 +4,7 @@ import com.kvendingoldo.jdcl.core.ConfigProcessor
 import com.kvendingoldo.jdcl.core.JenkinsLogger
 
 import hudson.FilePath
-import hudson.model.*
+import hudson.model.Executor
 
 import org.yaml.snakeyaml.*
 
@@ -40,16 +40,7 @@ class JobGenerator {
             logger.printLog "Reading: ${config}"
             configProcessor.processConfig(config.toString()).each { jobName, jc ->
                 if (jc.job.folder != '') {
-
-                    def build = Thread.currentThread().executable
-                    def currentBranch = build.buildVariableResolver.resolve('BRANCH')
-                    def finalFolder =  jc.job.folder
-
-                    if (currentBranch != jc.jenkins.mainJobDslBranch) {
-                        finalFolder = "${currentBranch}/${finalFolder}"
-                    }
-
-                    def list = finalFolder.split("/").toList()
+                    def list = jc.job.folder.split("/").toList()
                     def folderName = "${list[0]}"
                     dslFactory.folder(folderName)
 
